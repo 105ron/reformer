deeb5643308112c1e993bffa2fc3fd45ac13d4ce
rhys <rbrooker105@gmail.com>
2016-10-03 16:10:16 +1100

    new.html.erb form submits to database via html

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'users/new'
-   root 'users#new'
-		resources :users, only: [:new, :create]
+  root 'users#new'
+	resources :users, only: [:new, :create]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
